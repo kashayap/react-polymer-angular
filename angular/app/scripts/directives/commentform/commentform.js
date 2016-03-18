bc970a41f21be8753b7eb4f2ad3eadc06fcb8796
Kashayap Joshi <kashayap.joshi@gmail.com>
2016-03-17 22:27:56 -0500

    comitted

--- angular/app/scripts/directives/commentform/commentform.js
+++ angular/app/scripts/directives/commentform/commentform.js
@@ -9,7 +9,8 @@
 angular.module('commentForm', [])
   .directive('commentForm', function () {
     return {
-      template: '<form class="commentForm" name="form">' +
+      template: '<span id = "timestamp" ng-model="comment.timestamp">{{getDatetime()}}</span>' +
+	  '<form class="commentForm" name="form">' +
                   '<input type="text" placeholder="Your name" ng-model="comment.author" name="author"/>' +
                   '<input type="text" placeholder="Say something..." ng-model="comment.msg" name="msg"/>' +
                   '<input type="submit" value="Post" ng-click="submitComment()"/>' +
@@ -19,12 +20,17 @@ angular.module('commentForm', [])
         scope.comment = {};
         scope.submitComment = function(){
           var comment = scope.comment;
+		  comment.timestamp = document.getElementById('timestamp').innerHTML;
           if (!comment.msg || !comment.author) {
             return;
           }
           scope.$emit('submitted', comment);
           scope.comment = {};
         }
+		
+		scope.getDatetime = function() {
+		return (new Date);
+		};
       }
     };
   });
--- angular/app/scripts/directives/commentlist/commentlist.js
+++ angular/app/scripts/directives/commentlist/commentlist.js
@@ -9,16 +9,64 @@
 angular.module('commentList', ['comment'])
   .directive('commentList', function () {
     return {
-      template: '<div class="commentList">' +
+      template:'<div class="commentList">' +
                   '<comment-model ng-repeat="comment in comments" author="{{comment.author}}">' +
-                    '{{comment.msg}}' +
+                    '{{comment.msg}}' +'<span class="display_timestamp">{{getDatetime(comment.timestamp)}}</span>' +
                   '</comment-model>' +
                   '<span ng-if="comments.length < 1">No comments yet</span>' +
+				  
                 '</div>',
       restrict: 'E',
       scope: {
         comments: '='
       },
-      link: function postLink(scope, element, attrs) {}
+      link: function postLink(scope, element, attrs) {
+		  
+		scope.getDatetime = function(date1) {
+		
+			date1 = date1.replace(/^"(.*)"$/, '$1');
+			console.log(date1+" hi");
+	
+			var date = new Date(date1)
+		
+			var seconds = Math.floor((new Date() - date) / 1000);
+
+			var interval = Math.floor(seconds / 31536000);
+			if (interval > 1) {
+				return "-- " + interval + " years ago";
+			}
+			
+			interval = Math.floor(seconds / 2592000);
+			if (interval > 1) {
+				return "-- " + interval + " months ago";
+			}
+			
+			interval = Math.floor(seconds / 86400);
+			console.log("days");
+			console.log(interval);
+			if (interval > 1) {
+				return "-- " + interval + " days ago";
+			}
+			
+			interval = Math.floor(seconds / 3600);
+			if (interval > 1) {
+				return "-- " + interval + " hours ago";
+			}
+			
+			interval = Math.floor(seconds / 60);
+			if (interval > 1) {
+				if (interval > 60){
+					return "-- 1 hour ago";
+				}
+				else{
+					return "-- " + interval + " minutes ago";
+				}
+				
+			}
+			//return Math.floor(seconds) + " seconds ago";
+			return "-- 1 minute ago";
+
+		 }
+	  }
     };
   });
